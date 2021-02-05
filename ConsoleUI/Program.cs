044d28ee07a1f950418d60787ef160689012bc2f
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-02-05 03:17:22 +0300

    3 Şubat 2021

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,16 +1,19 @@
 ﻿using Business.Concrete;
 using System;
 using DataAccess.Concrete.InMemory;
+using DataAccess.Concrete.EntityFrameWork;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //Open Closed Principle
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(40,100))
             {
                 Console.WriteLine(product.ProductName);
             }
---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Entities.Abstract;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint - generic kısıtlama
+    //class : referans tip olabilir demek sınıf olabilir demek değil
+    //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+
+    }
+}
