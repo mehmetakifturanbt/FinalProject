63d9e60a9540d78a6cc70a7e8ac3f1fd3b7b824c
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-02-07 03:33:34 +0300

    6 Şubat 2021

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
-using Entities.Abstract;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //generic constraint - generic kısıtlama
     //class : referans tip olabilir demek sınıf olabilir demek değil
