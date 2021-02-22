1bd05d3228d4b5fb4fc8265050f99e244c4b0b87
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-02-22 03:41:41 +0300

    17 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 
 namespace Business.Concrete
 {
@@ -20,15 +24,33 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            //business codes
+            
+            //business codes - iş kodları
+            //validation codes - doğrulama kodları
 
-            if (product.ProductName.Length < 2)
-            {
-                //magic strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+            //if (product.UnitPrice<=0)
+            //{
+            //    return new ErrorResult(Messages.UnitPriceInvalid);
+            //}
+
+            //if (product.ProductName.Length < 2)
+            //{
+            //    //magic strings
+            //    return new ErrorResult(Messages.ProductNameInvalid);
+            //}
+
+            //ValidationTool.Validate(new ProductValidator(), product);
+            //Loglama
+            //cacheremove
+            //performance
+            //transaction
+            //yetkilendirme
+
+            //business codes
 
             _productDal.Add(product);
 
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,9 +32,10 @@ namespace WebAPI
 
             //Autofac, Ninject, CastleWindsor, StructureMap, LightInject, DryInject --> IoC Container
             //AOP 
+            //Postsharp - Çek yapýmý, ücretlidir
             services.AddControllers();
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
