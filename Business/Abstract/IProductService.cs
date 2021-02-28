f586263d042ffb436a75f2032b0c3ac433384b6b
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-02-28 18:38:38 +0300

    24 Şubat Ders Sonu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -14,7 +14,7 @@ namespace Business.Abstract
         IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productId);
-
         IResult Add(Product product);
+        IResult Update(Product product);
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Business.Abstract;
+using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
@@ -18,10 +21,14 @@ namespace Business.Concrete
     public class ProductManager : IProductService
     {
         IProductDal _productDal; //Soyut nesne ile bağlantı kuracağız. Ne inmemory ismi geçecek ne de entity frame work ismi geçecek
+        ICategoryService _categoryService;
 
-        public ProductManager(IProductDal productDal)
+        public ProductManager(IProductDal productDal,ICategoryService categoryService)
         {
             _productDal = productDal;
+            _categoryService = categoryService;
+
+
         }
 
 
@@ -29,32 +36,48 @@ namespace Business.Concrete
         public IResult Add(Product product)
         {
             
-            //business codes - iş kodları
-            //validation codes - doğrulama kodları
+                //business codes
+                //Bir kategoride en fazla 10 ürün olabilir
+                //Aynı isimde ürün eklenemez.
+                //Eğer mevcut kategori sayısı 15'i geçtiyse sisteme yeni ürün eklenemez.
 
-            //if (product.UnitPrice<=0)
-            //{
-            //    return new ErrorResult(Messages.UnitPriceInvalid);
-            //}
+                IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
+                    CheckIfProductCountOfCategoryCorrect(product.CategoryId),CheckIfCategoryLimitExceded());
 
-            //if (product.ProductName.Length < 2)
-            //{
-            //    //magic strings
-            //    return new ErrorResult(Messages.ProductNameInvalid);
-            //}
+                if (result!=null)
+                {
+                    return result;
+                }
 
-            //ValidationTool.Validate(new ProductValidator(), product);
-            //Loglama
-            //cacheremove
-            //performance
-            //transaction
-            //yetkilendirme
+                _productDal.Add(product);
 
-            //business codes
+                return new SuccessResult(Messages.ProductAdded);
 
-            _productDal.Add(product);
+            
 
-            return new SuccessResult(Messages.ProductAdded);
+
+                //business codes - iş kodları
+                //validation codes - doğrulama kodları
+
+                //if (product.UnitPrice<=0)
+                //{
+                //    return new ErrorResult(Messages.UnitPriceInvalid);
+                //}
+
+                //if (product.ProductName.Length < 2)
+                //{
+                //    //magic strings
+                //    return new ErrorResult(Messages.ProductNameInvalid);
+                //}
+
+                //ValidationTool.Validate(new ProductValidator(), product);
+                //Loglama
+                //cacheremove
+                //performance
+                //transaction
+                //yetkilendirme
+
+
         }
 
         public IDataResult<List<Product>> GetAll()
@@ -94,6 +117,55 @@ namespace Business.Concrete
             }
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
 
+        }
+
+        [ValidationAspect(typeof(ProductValidator))]
+        public IResult Update(Product product)
+        {
+            var result = _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
+
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+            throw new NotImplementedException();
+        }
+
+        private IResult CheckIfProductCountOfCategoryCorrect(int categoryId)
+        {
+            //Select count(*) from products where categoryId=1
+            var result = _productDal.GetAll(p => p.CategoryId == categoryId).Count;
+
+            if (result >= 10)
+            {
+                return new ErrorResult(Messages.ProductCountOfCategoryError);
+            }
+
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            //Select count(*) from products where categoryId=1
+            var result = _productDal.GetAll(p => p.ProductName == productName).Any();
+
+            if (result)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfCategoryLimitExceded()
+        {
+            var result = _categoryService.GetAll();
+            if (result.Data.Count>15)
+            {
+                return new ErrorResult(Messages.CategoryLimitExceded);
+            }
+
+            return new SuccessResult();
         }
     }
 }
