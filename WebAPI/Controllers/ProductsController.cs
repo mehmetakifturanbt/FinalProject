c6c17eac669194475de358fa54b54a8220e77a1f
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-03-13 22:27:51 +0300

    13 Mart Commit 1

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
@@ -32,6 +33,9 @@ namespace WebAPI.Controllers
             //Swagger
             //Dependency chain -- bağımlılık zinciri
             
+            Thread.Sleep(1000);
+
+
             var result = _productService.GetAll();
             if (result.Success)
             {
