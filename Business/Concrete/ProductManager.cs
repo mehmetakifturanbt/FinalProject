ffb2ce78f8b8c41102c0e7bad67de38464bd6542
Mehmet Akif Turan <mehmetakifturan@gmail.com>
2021-03-21 17:50:44 +0300

    20 Mart Commit

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -36,7 +36,7 @@ namespace Business.Concrete
         }
 
         //Claim - İddia etmek
-        [SecuredOperation("product.add,admin")]
+        //[SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
